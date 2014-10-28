e865bd605b25cc5b61801b963292b6fb6a08ef2a
paco <paco@paco.(none)>
2014-10-28 08:18:42 +0100

    Added new files

--- graficos_estadisticos_1.r
+++ graficos_estadisticos_1.r
@@ -1,4 +1,4 @@
-##' # Gráficos estadísticos con R
+##' # Gráficos estadísticos con R (1)
 ##' [Francisco García García](http://bioinfo.cipf.es/ "webpage of department") _(2014-10-29)_  
 ##'
 ##' 
@@ -7,16 +7,19 @@
 
 
 
-##' ## 1. Gráficos para una variable cuantitativa 
+##' ## 1. Representación gráfica de una variable cuantitativa 
 
 #esto es una linea comentada
 
-##' Nos gustaría representar gráficamente los valores de la siguiente variable cuantitativa, utilizaremos los _boxplots_:
+##' Queremos representar gráficamente los valores de la variable colesterol. Utilizaremos los _diagramas de cajas o boxplots_:
 colesterol <- c(150,125,138,144,150,201,225,233,200,190, 250,150,180,240,235)  #incluimos los datos de nuestra variable
 boxplot(colesterol)
 
 ##' Es posible incluir o modificar elementos en el gráfico:
 boxplot(colesterol, col = "red")                                  #color 
+
+##' con un interrogante delante del nombre de la función obtenemos ayuda sobre su uso
+?boxplot   
 
 boxplot(colesterol, col = "red", main = "Exp. 53",
         xlab = "Colesterol")  #título principal y subtítulos para ejes 
@@ -52,13 +55,44 @@ abline(v = 175, col = "green", lwd = 2)
 
 
 
-##' ## 2. Gráficos para una variable cualitativa o categórica
+##' ## 2. Representación gráfica de una variable categórica 
+##' Gráficos circulares o de sectores:
+ 
+##' La variable "variante" incluye las frecuencias de diferentes tipos de mutaciones:
+variante  <- c(10,12,4,16)
+nombres   <- c("SNP", "stop lost", "intron variant", "exon variant")
+
+#calculamos los porcentajes
+pct <- round(variante/sum(variante)*100)
+
+#realizamos el gráfico:
+pie(variante)
+pie(variante, labels = nombres)  #añadimos nombres de las categorias
+pie(variante, labels = paste(nombres, pct, "%"))  #añadimos nombres de las categorias + porcentajes
+pie(variante, labels = paste(nombres, pct, "%"), col = c("red", "blue", "pink", "green"))  #cambiamos colores
+pie(variante, labels = paste(nombres, pct, "%"), col = c("red", "blue", "pink", "green"),
+    main = "Tipos de variantes genómicas") #incluimos un título 
+
+ 
+##' Gráficos de barras:
+##' Este tipo de gráficos son útiles para representar variables categóricas o variables cuantitativas discretas
+##' La variable "mut" indica el número de mutaciones encontradas en varios genes de interés (varía entre 1 y 5)
+
+mut <- c(1,3,5,2,4,4,4,1,1,1,1,3,5,2)  #datos iniciales
+datos <- table(mut) #tabla de frecuencias
+barplot(datos) #generamos el diagrama de barras
+
+barplot(datos, xlab = "número de mutaciones", ylab = "frecuencias", main = "Mutaciones detectadas en genes OPITZ")
+#hemos incluido título del gráfico  y títulos para los ejes X,Y.
+barplot(datos, horiz = TRUE, 
+        ylab = "número de mutaciones", xlab = "frecuencias", main = "Mutaciones detectadas en genes OPITZ", 
+        col = "green")  #cambiamos el modo de visualización y añadimos color a las barras
 
 
 
 
-
-##' ## 3. Representación gráfica de dos variables cuantitativas (diagramas de dispersión o de puntos):
+##' ## 3. Representación gráfica de dos variables cuantitativas
+##' 
 ##' Queremos conocer la relacion entre el nivel de cuantificación de varios metabolitos
 
 #introducimos los datos de 3 metabolitos:
@@ -66,7 +100,7 @@ glutamato <- c(143, 90, 100, 119,  98,  87,  84,  99, 101,  99)
 colina <- c(146, 97, 106, 123, 103,  90,  90, 106, 108, 101)  
 HLDL <-   c(100, 107, 105, 89, 88, 102, 122, 105, 93, 145)
 
-#¿qué relación hay entre la colina y el HLDL? 
+#¿qué relación hay entre la colina y el HLDL? Utilizaremos los gráficos de dispersión o de puntos
 plot(colina, HLDL)
 
 plot(colina, HLDL, pch = 2, col = "blue", main = "colina vs. HLDL")      #representamos cada metabolito con triángulos azules 
@@ -82,355 +116,23 @@ plot(colina, glutamato, main = "colina vs. glutamato", cex=0.5, col = "blue") #p
 
 #¿podemos ver las relaciones de los tres metabolitos por parejas?
 pairs(~glutamato + colina + HLDL)
-pairs(~glutamato + colina + HLDL)
+
+pairs(~glutamato + colina + HLDL, col = "blue", main = "Correlación lineal entre metabolitos")
+#incluyendo nuevos elementos en el gráfico
 
 
 
 
 
 
-
-
-
-
-
-
-
-##' ## Links interesantes con información complementaria
-##' 
-##' [Quick-R] (http://www.statmethods.net/)   Muy directo!
 ##'
-##' [ggplot2] (http://ggplot2.org/)  Paquete de funciones que personalizan cualquier tipo de gráfico.
-
-
-
-##' R se puede utilizar como calculadora
-2+2
-2*3
-
-##' La asignacion de valores se hace utilizando: `<-` 
-variableA <- 3
-variableA
-variableA * 2
-
-##' Todas las funciones en R van siempre seguidas de parentesis
-##' que envuelven los argumentos de la funcion
-sqrt (3)
-sqrt (variableA)
-SQRT (variableA)   #R es _case sensitive_ no funciona en mayusculas
-variableA
-VARIABLEA
-sqrt ("variableA") #cada funcion esta definida para un tipo o clase de argumentos
-
-
-
-
-
-##' ## Clases de variables; la FUNCION class
-class (1)
-class ("adjgfagkjg")
-class ("1")
-class (TRUE); class (FALSE)
-class (factor (1))
-
-##' Las funciones tambien son 'objetos' accesibles en la sesion
-class (ls)
-ls () # llamada a la funcion: con parentesis, aunque no tenga argumentos
-ls    # llamada a la funcion sin parentesis: muestra el objeto, es decir, el codigo de la función.
-
-
-
-
-
-##' ## VECTORES
-
-##' Varios elementos se pueden concatenar en un vector utilizando el operador o funcion: `c ()`
-c (1,2,3)
-c ("a","b","c")
-c ("a","b","c", 1,2,3) #OBS: todos los elementos del vector son del mismo tipo
-
-
-##' Todos los elementos del vector tienen la misma clase
-class (c (1,2,3))
-class (c ("a","b","c"))
-class (c ("a","b","c", 1,2,3)) #Si no R cambia el tipo de alguno de ellos.
-
-
-##' La longitud es una caracteristica de los vectores que se podemos
-##' conocer utilizando la funcion length
-length (c (1,2,3))
-length (c ("a","b","c"))
-length (c ("a","b","c", 1,2,3))
-
-
-##' Todas las variables en R son vectores de longitud 1
-is.vector (1)
-class (1)
-length (1)
-
-is.vector ("adjgfagkjg")
-class ("adjgfagkjg")
-length ("adjgfagkjg")
-
-is.vector (TRUE)
-class (TRUE)
-length (TRUE)
-
-
-##' Son lo mismo:
-1
-c (1)
-
-1 == c (1)
-identical (1, c (1))
-
-
-##' Asignacion de valores a variables
-a <- 4
-a
-b <- c (1,2,3)
-b
-c <- c (5,4,3,2,1)
-c
-d <- c ("a","b","c", "d", "e", "f")
-d
-
-length (a)
-length (b)
-length (c)
-length (d)
-
-
-##' Se puede reasignar valores
-a
-a <- 7
-a
-a <- c ("a","b","c",)
-a
-
-
-##' Los vectores pueden ser de varias CLASES
-class (1:10)               #"integer"
-class (c (1.0, 2.0, 3.0))  #"numeric"
-class (c ("A", "B", "C"))  #"character"
-class (c (TRUE, FALSE))    #"logical"
-
-
-##' Se puede acceder a los elementos del vector por su poscion
-d
-d[1]
-d[3]
-
-
-##' Varias posiciones del vector se indican, a su vez, como un vector numerico
-d[c (1,2,3)]
-d[c (3,2,1)]  #sigue siendo un vector en otro orden
-
-
-##' quizas sea mas claro asi presentado
-v <- c (3,2,1)
-class (v)  ##es un vector
-d[v]       ##que 'indexa' a otro vector
-
-class (d[c (1,2,3)])  #es un vector nuevo
-length (d[c (1,2,3)]) #con longitud diferente
-
-
-##' Se puede acceder a todo el vector menos algunas posiciones 
-d
-d[-1]
-
-d[c (-1,-3,-5)]
-d[ - c (1,3,5)] # el operador - se aplica a todos los elementos del vector
-d[c (1, 3, -5)] # no se pueden mezclar las dos formas de acceso
-
-
-##' El operador ':' indica una secuencia de enteros
-10:15
-c (10, 11, 12 ,13 ,14, 15)
-
-identical (10:15, c(10, 11, 12 ,13 ,14, 15))
-
-class (10:15)
-class (c(10, 11, 12 ,13 ,14, 15))
-
-##' Al ser un vector, se puede usar para indexar las posiciones de otros
-d[3:1]
-
-
-##' OBS: los vectores tambien se pueden nombrar
-v <- 101:103
-v
-names (v)
-names (v) <- c ("n1", "n2", "n3")
-v
-names (v)
-
-##' de hecho se pueden nombrar al definirlos
-c (N1 = "primer valor", N2 = "segundo valor")
-
-
-##' Podemos acceder a las posiciones de los vectores por sus nombres si las tienen
-v[2]
-v["n2"]
-
-v[3:2]
-v[c("n3", "n2")]
-
-##' Tambien se puede acceder a los elementos de un vector indexando con vectores 'logicos'
-v
-
-logico <- c(TRUE, FALSE, TRUE)
-is.vector (logico)
-class (logico)
-
-v[logico]
-
-
-##' ## Operaciones entre vectores
-
-##' Las operaciones entre vectores se realizan entre los elementos de vectores segun la posicion
-
-c (1,1,2,3) + c (4,5,6,9)
-
-A <- c (1,1,2,3)
-B <- c (4,5,6,9)
-
-A + B
-A * B
-B / A
-
-##' potencias
-A^2
-A^3
-
-##' potencias (forma alternativa)
-A**2
-
-
-3*A ##' es tambien una operacion entre elementos de vectores
-##' el vector c(3) se extiende automaticamente
-
-1/A
-1/B
-
-
-##' Generalmente, vectores de diferente longitud se _reciclan_ en las operaciones
-A
-c (2,3) * A
-
-c (3,3) * c (1,2,3,4)
-
-c (1,2) * c (1,2,3,4) #es lo mismo que:
-c (1,2,1,2) * c (1,2,3,4)
-
-
-##' La longitud de uno de los vectores ha de ser multiplo de la longitud del otro vector.
-##' Este tipo de repeticiones suele funcionar con todas las operaciones entre vectores.
-c (1,2,1) * c (1,2,3,4) 
-
-
-##' Missing values: NA (_not available_)
-m <- c (1:2, NA, 4:6)
-m
-class (m)
-class (m[3])
-m[3]
-
-n <- c ("a", NA, "b")
-n
-class (n)
-class (n[2])
-n[2]
-
-is.na (m)
-is.na (n)
-
-m
-m+1 #los NA se propagan en las operaciones
-
-
-
-##' No todas las _operaciones_ entre vectores son _numericas_.
-##' Por ejemplo la funcion para concatenar textos:
-paste ("texto primero", "texto segundo", sep = " ")
-
-paste ("texto primero", "texto segundo", sep = "_")
-paste ("texto primero", "texto segundo", sep = " Y ")
-
-paste ( c("texto1", "texto2"), c("TEXTO1", "TEXTO2", "TEXTO3", "TEXTO4"), sep = " ")
-
-
-
-##' Tambien hay operaciones _logicas_ definidas para los vectores logicos
-vectorlogico <- is.na (m)
-vectorlogico
-class (vectorlogico)
-length (vectorlogico)
-vectorlogico[1:3]
-
-
-##' Operadres logicos: NEGACION
-!TRUE
-!FALSE
-!vectorlogico       ##afectan a todos los elementos del vector
-
-
-##' Operadres logicos: Y
-TRUE  & TRUE
-TRUE  & FALSE
-FALSE & FALSE
-c (TRUE, TRUE) & c (TRUE, FALSE)
-
-
-##' Operadres logicos: O
-TRUE | TRUE
-TRUE | FALSE
-FALSE | FALSE
-c (TRUE, TRUE) | c (TRUE, FALSE)
-
-
-
-
-
-
-##' ## LOOPS con vectores
-v
-
-##' Iteramos sobre las posiciones:
-posiciones <- 1:3
-posiciones
-for (i in posiciones) {
-  cat ("\n") #linea nueva
-  ##
-  print (i)
-  dato <- v[i]
-  print (dato)
-}
-
-##' Iteramos sobre los nombres del vector:
-nombres <- names (v) 
-nombres
-for (i in nombres) {
-  cat ("\n") #linea nueva
-  print (i)
-  dato <- v[i]
-  print (dato)
-}
-
-##' Iteramos __sobre los propios elementos__ del vector.
-##' Es una forma particular de R (no esta en algunos otros lenguajes)
-for (i in v) {
-  cat ("\n") #linea nueva
-  print (i)
-  dato <- i
-  print (dato)
-}
-
-
-##' Funciones que leen y escriben vectores:
-##' - readLines
-##' - writeLines
+##' ## Links interesantes:
 ##' 
+##' -[Quick-R] (http://www.statmethods.net/)   Muy directo!
+##'
+##' -[ggplot2] (http://ggplot2.org/)  Paquete de funciones que personalizan cualquier tipo de gráfico.
+##' 
+##' -[r-tutor] (http://www.r-tutor.com/)  An R Introduction to Statistics
+##'
 
 ##' ----------------------------------------------------------------------------
